11e5ff37ce728b61a744a046f5e22889199ddeff
Rheanne McIntosh <rheanne.mcintosh@visualsoft.co.uk>
2022-02-03 21:48:56 +0000

    Add example of separating status code and set header

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ const http = require('http');
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, {
-        'Content-Type': 'application/json',
-    });
     res.end(JSON.stringify({
         id: 1,
         name:'Sir Isaac Netwon',
     }));
+        // res.writeHead(200, {
+        //     'Content-Type': 'application/json',
+        // });
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
 });
 
 server.listen(PORT, () =>{
